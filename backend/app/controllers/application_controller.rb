aa511bc81067cd91f705c9ec5e7474cf8d813989
Nick Pupko <work.pupko@gmail.com>
2023-06-24 12:38:32 +0300

    Add services system

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,11 +1,9 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  delegate :resolve, to: :container
+
   def container
     Container
-  end
-
-  def resolve
-    container.injector
   end
 end
--- backend/app/services/hello.rb
+++
@@ -1,7 +0,0 @@
-class Hello
-  # include Core::Service
-
-  def call(name)
-    Success("Hello, #{name}!")
-  end
-end
