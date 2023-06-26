0e449abd438e651aa5b1bb8277d5597d271dd41f
Nick Pupko <work.pupko@gmail.com>
2023-06-26 18:47:59 +0300

    [COD-60] Apply Rubocop rules

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -1,14 +1,14 @@
-require_relative "boot"
+# frozen_string_literal: true
 
-require "rails/all"
+require_relative 'boot'
+
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-if ['development', 'test'].include? ENV['RAILS_ENV']
-  Dotenv::Railtie.load
-end
+Dotenv::Railtie.load if %w[development test].include? ENV['RAILS_ENV']
 
 module Skillbridge
   class Application < Rails::Application
--- backend/system/container.rb
+++ backend/system/container.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 require_relative '../lib/core/service'
 
 # Load container and inject dependencies
