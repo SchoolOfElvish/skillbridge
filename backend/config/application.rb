e86a0fc935febdb2be3d9e2edaee8a4708c01374
Nick Pupko <work.pupko@gmail.com>
2023-06-26 19:04:06 +0300

    [COD-60] Add Houdini GraphQL client

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -22,5 +22,6 @@ module Skillbridge
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.hosts << "backend"
   end
 end
---
+++ frontend/houdini.config.js
@@ -0,0 +1,13 @@
+/// <references types="houdini-svelte">
+
+/** @type {import('houdini').ConfigFile} */
+const config = {
+    "watchSchema": {
+        "url": "http://backend:3000/graphql"
+    },
+    "plugins": {
+        "houdini-svelte": {}
+    }
+}
+
+export default config
